19b1d6698a05a796cf1f99fdfb5d252effa53b6f
VKartsev <vach-partner@mail.ru>
2022-08-12 00:14:33 +0300

    Project Inheritance and extensibility of systems. Inheritance problems

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -3,7 +3,7 @@ package ru.netology;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerTest {
     private ProductRepository repository = new ProductRepository();
